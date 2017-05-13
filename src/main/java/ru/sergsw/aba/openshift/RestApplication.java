3beedb5c0f964733ce64df701e7941ed4deccf51
Serg <serg@sergsw.ru>
2017-05-13 16:15:36 +0300

    fix

--- src/main/java/ru/sergsw/aba/openshift/RestApplication.java
+++ src/main/java/ru/sergsw/aba/openshift/RestApplication.java
@@ -3,6 +3,6 @@ package ru.sergsw.aba.openshift;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("api")
+@ApplicationPath("/api")
 public class RestApplication extends Application {
 }
--- src/main/java/ru/sergsw/aba/openshift/rest/AuthResource.java
+++ src/main/java/ru/sergsw/aba/openshift/rest/AuthResource.java
@@ -9,13 +9,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
-@Path("auth")
+@Path("/auth")
 @Produces("application/json")
 public class AuthResource {
 
     private @Inject AuthService authService;
 
-    @Path("login/{login}")
+    @Path("/login/{login}")
     @POST
     public String auth(@PathParam("login") String login, String password) throws LoginException {
         return authService.login(login, password);
