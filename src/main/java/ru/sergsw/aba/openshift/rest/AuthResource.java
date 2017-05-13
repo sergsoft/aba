56d091539c6439fc654f9b39ec402f7d61e6521d
Serg <serg@sergsw.ru>
2017-05-13 16:02:14 +0300

    fix

--- src/main/java/ru/sergsw/aba/openshift/rest/AuthResource.java
+++ src/main/java/ru/sergsw/aba/openshift/rest/AuthResource.java
@@ -3,7 +3,8 @@ package ru.sergsw.aba.openshift.rest;
 import ru.sergsw.aba.openshift.services.AuthService;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
+import javax.security.auth.login.LoginException;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -15,8 +16,8 @@ public class AuthResource {
     private @Inject AuthService authService;
 
     @Path("login/{login}")
-    @GET
-    public String auth(@PathParam("login") String login, String password) {
+    @POST
+    public String auth(@PathParam("login") String login, String password) throws LoginException {
         return authService.login(login, password);
     }
 }
